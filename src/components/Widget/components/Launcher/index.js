a02b01856446364f34910de255d6b4cb804d7346
Matthieu Joannon <mattjoannon@gmail.com>
2020-05-22 16:58:38 -0400

    chore: test

--- src/components/Widget/components/Launcher/index.js
+++ src/components/Widget/components/Launcher/index.js
@@ -35,6 +35,8 @@ const Launcher = ({
     const setReference = (selector) => {
       const reference = document.querySelectorAll(selector);
       if (reference && reference.length === 1) {
+        console.log(reference.length);
+        console.log(reference);
         setReferenceElement(reference[0]);
       } else {
         setReferenceElement(null);
